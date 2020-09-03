a5ea048b4e86b38097e8a52a5f6c65a5fa187c79
nuvallo <nuvallo.inc@gmail.com>
2020-09-02 21:50:34 -0400

    bug fix for printing

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print("x is %d, y is %d, z is %s" % (x, round(y, 2), z))
+print("x is %s, y is %s, z is %s" % (x, round(y, 2), z))
 
 # Use the 'format' string method to print the same thing
 print("x is {x}, y is {y}, z is {z}".format(x=x, y=round(y,2), z=z))
